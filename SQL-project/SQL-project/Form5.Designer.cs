b3b004e802fd097d084567a06641f37af2c52af2
Henipiter <henipiter@wp.pl>
2021-01-17 15:48:47 +0100

    almost donegit add .

--- SQL-project/SQL-project/Form5.Designer.cs
+++ SQL-project/SQL-project/Form5.Designer.cs
@@ -36,17 +36,15 @@
             this.ResultBox = new System.Windows.Forms.TextBox();
             this.CountCarEarnings = new System.Windows.Forms.Button();
             this.CountCarValue = new System.Windows.Forms.Button();
-            this.label2 = new System.Windows.Forms.Label();
-            this.PopularCarsButton1 = new System.Windows.Forms.Button();
-            this.Top5carsButton = new System.Windows.Forms.Button();
             this.CancelButton = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // CountCarsButton
             // 
-            this.CountCarsButton.Location = new System.Drawing.Point(63, 67);
+            this.CountCarsButton.Location = new System.Drawing.Point(84, 82);
+            this.CountCarsButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CountCarsButton.Name = "CountCarsButton";
-            this.CountCarsButton.Size = new System.Drawing.Size(126, 39);
+            this.CountCarsButton.Size = new System.Drawing.Size(168, 48);
             this.CountCarsButton.TabIndex = 0;
             this.CountCarsButton.Text = "Cars";
             this.CountCarsButton.UseVisualStyleBackColor = true;
@@ -54,9 +52,10 @@
             // 
             // CountRepairButton
             // 
-            this.CountRepairButton.Location = new System.Drawing.Point(195, 68);
+            this.CountRepairButton.Location = new System.Drawing.Point(260, 84);
+            this.CountRepairButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CountRepairButton.Name = "CountRepairButton";
-            this.CountRepairButton.Size = new System.Drawing.Size(126, 37);
+            this.CountRepairButton.Size = new System.Drawing.Size(168, 46);
             this.CountRepairButton.TabIndex = 1;
             this.CountRepairButton.Text = "Car Repairs";
             this.CountRepairButton.UseVisualStyleBackColor = true;
@@ -64,9 +63,10 @@
             // 
             // CountCarCosts
             // 
-            this.CountCarCosts.Location = new System.Drawing.Point(327, 67);
+            this.CountCarCosts.Location = new System.Drawing.Point(436, 82);
+            this.CountCarCosts.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CountCarCosts.Name = "CountCarCosts";
-            this.CountCarCosts.Size = new System.Drawing.Size(126, 39);
+            this.CountCarCosts.Size = new System.Drawing.Size(168, 48);
             this.CountCarCosts.TabIndex = 2;
             this.CountCarCosts.Text = "Car Costs";
             this.CountCarCosts.UseVisualStyleBackColor = true;
@@ -74,33 +74,37 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(63, 41);
+            this.textBox1.Location = new System.Drawing.Point(84, 50);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(390, 20);
+            this.textBox1.Size = new System.Drawing.Size(519, 22);
             this.textBox1.TabIndex = 3;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(22, 80);
+            this.label1.Location = new System.Drawing.Point(29, 98);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
+            this.label1.Size = new System.Drawing.Size(45, 17);
             this.label1.TabIndex = 4;
             this.label1.Text = "Count";
             // 
             // ResultBox
             // 
-            this.ResultBox.Location = new System.Drawing.Point(25, 163);
+            this.ResultBox.Location = new System.Drawing.Point(33, 201);
+            this.ResultBox.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.ResultBox.Multiline = true;
             this.ResultBox.Name = "ResultBox";
-            this.ResultBox.Size = new System.Drawing.Size(736, 263);
+            this.ResultBox.Size = new System.Drawing.Size(980, 323);
             this.ResultBox.TabIndex = 5;
             // 
             // CountCarEarnings
             // 
-            this.CountCarEarnings.Location = new System.Drawing.Point(459, 68);
+            this.CountCarEarnings.Location = new System.Drawing.Point(612, 84);
+            this.CountCarEarnings.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CountCarEarnings.Name = "CountCarEarnings";
-            this.CountCarEarnings.Size = new System.Drawing.Size(126, 39);
+            this.CountCarEarnings.Size = new System.Drawing.Size(168, 48);
             this.CountCarEarnings.TabIndex = 6;
             this.CountCarEarnings.Text = "Car Earnings";
             this.CountCarEarnings.UseVisualStyleBackColor = true;
@@ -108,48 +112,21 @@
             // 
             // CountCarValue
             // 
-            this.CountCarValue.Location = new System.Drawing.Point(591, 67);
+            this.CountCarValue.Location = new System.Drawing.Point(788, 82);
+            this.CountCarValue.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CountCarValue.Name = "CountCarValue";
-            this.CountCarValue.Size = new System.Drawing.Size(126, 39);
+            this.CountCarValue.Size = new System.Drawing.Size(168, 48);
             this.CountCarValue.TabIndex = 7;
             this.CountCarValue.Text = "Car Value";
             this.CountCarValue.UseVisualStyleBackColor = true;
             this.CountCarValue.Click += new System.EventHandler(this.CountCarValue_Click);
             // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(23, 131);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(34, 13);
-            this.label2.TabIndex = 8;
-            this.label2.Text = "Show";
-            // 
-            // PopularCarsButton1
-            // 
-            this.PopularCarsButton1.Location = new System.Drawing.Point(63, 118);
-            this.PopularCarsButton1.Name = "PopularCarsButton1";
-            this.PopularCarsButton1.Size = new System.Drawing.Size(126, 39);
-            this.PopularCarsButton1.TabIndex = 9;
-            this.PopularCarsButton1.Text = "Top 1 car";
-            this.PopularCarsButton1.UseVisualStyleBackColor = true;
-            this.PopularCarsButton1.Click += new System.EventHandler(this.PopularCarsButton1_Click);
-            // 
-            // Top5carsButton
-            // 
-            this.Top5carsButton.Location = new System.Drawing.Point(195, 118);
-            this.Top5carsButton.Name = "Top5carsButton";
-            this.Top5carsButton.Size = new System.Drawing.Size(126, 39);
-            this.Top5carsButton.TabIndex = 10;
-            this.Top5carsButton.Text = "Top 5 cars";
-            this.Top5carsButton.UseVisualStyleBackColor = true;
-            this.Top5carsButton.Click += new System.EventHandler(this.Top5carsButton_Click);
-            // 
             // CancelButton
             // 
-            this.CancelButton.Location = new System.Drawing.Point(591, 118);
+            this.CancelButton.Location = new System.Drawing.Point(788, 145);
+            this.CancelButton.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.CancelButton.Name = "CancelButton";
-            this.CancelButton.Size = new System.Drawing.Size(126, 39);
+            this.CancelButton.Size = new System.Drawing.Size(168, 48);
             this.CancelButton.TabIndex = 11;
             this.CancelButton.Text = "Cancel";
             this.CancelButton.UseVisualStyleBackColor = true;
@@ -157,13 +134,10 @@
             // 
             // Form5
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1067, 554);
             this.Controls.Add(this.CancelButton);
-            this.Controls.Add(this.Top5carsButton);
-            this.Controls.Add(this.PopularCarsButton1);
-            this.Controls.Add(this.label2);
             this.Controls.Add(this.CountCarValue);
             this.Controls.Add(this.CountCarEarnings);
             this.Controls.Add(this.ResultBox);
@@ -172,6 +146,7 @@
             this.Controls.Add(this.CountCarCosts);
             this.Controls.Add(this.CountRepairButton);
             this.Controls.Add(this.CountCarsButton);
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "Form5";
             this.Text = "Form5";
             this.ResumeLayout(false);
@@ -189,9 +164,6 @@
         private System.Windows.Forms.TextBox ResultBox;
         private System.Windows.Forms.Button CountCarEarnings;
         private System.Windows.Forms.Button CountCarValue;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.Button PopularCarsButton1;
-        private System.Windows.Forms.Button Top5carsButton;
         private System.Windows.Forms.Button CancelButton;
     }
 }
--- SQL-project/SQL-project/Form5.cs
+++ SQL-project/SQL-project/Form5.cs
@@ -7,6 +7,8 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Oracle.ManagedDataAccess.Client;
+using Oracle.ManagedDataAccess.Types;
 
 namespace SQL_project
 {
@@ -18,45 +20,75 @@ namespace SQL_project
             InitializeComponent();
             mainForm = callingForm as Form2;
         }
+        private void callFunction(string nameFunction, string argument)
+        {
+            if (argument.Length > 0)
+            {
+                argument = "'" + argument + "'";
+            }
+            OracleCommand cmd = mainForm.mainForm.con.CreateCommand();
+            //select ilenaprawdanysamochod('aaaaaaaaaaaaaaaaa') from dual;
+            cmd.CommandText = "select "+nameFunction+"('"+argument+"') from dual" ;
+            OracleDataReader reader = cmd.ExecuteReader();
+            if (reader.HasRows)
+            {
+                this.ResultBox.Text = "";
+                while (reader.Read())
+                {
+                    for (int i = 0; i < reader.FieldCount; i++)
+                    {
+                        this.ResultBox.Text += reader.GetString(i) + "; ";
+                    }
+                    this.ResultBox.Text += "\n";
+                }
+            }
+            else
+            {
+                Console.WriteLine("No rows found.");
+                this.ResultBox.Text = "Brak wierszy";
+            }
+            reader.Close();
+        }
 
         private void CountCarsButton_Click(object sender, EventArgs e)
         {
-
+            string func = "ilesamochodow";
+            string arg = this.textBox1.Text;
+            callFunction(func, arg);
         }
 
         private void CountRepairButton_Click(object sender, EventArgs e)
         {
-
+            string func = "ilenaprawdanysamochod";
+            string arg = this.textBox1.Text;
+            callFunction(func, arg);
         }
 
         private void CountCarCosts_Click(object sender, EventArgs e)
         {
-
+            string func = "lacznykoszt";
+            string arg = this.textBox1.Text;
+            callFunction(func, arg);
         }
 
         private void CountCarEarnings_Click(object sender, EventArgs e)
         {
-
+            string func = "lacznyzyskzwynajmu";
+            string arg = this.textBox1.Text;
+            callFunction(func, arg);
         }
 
         private void CountCarValue_Click(object sender, EventArgs e)
         {
-
+            string func = "ilezarobil";
+            string arg = this.textBox1.Text;
+            callFunction(func, arg);
         }
 
-        private void PopularCarsButton1_Click(object sender, EventArgs e)
-        {
-
-        }
-
-        private void Top5carsButton_Click(object sender, EventArgs e)
-        {
-
-        }
 
         private void CancelButton_Click(object sender, EventArgs e)
         {
-
+            this.Close();
         }
     }
 }
